0ce33aaa8da018ff3d79bc42688b799257624601
AnastasyMeleshko <7634907@mail.ru>
2022-01-26 13:42:19 +0300

    reinstall eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    amd: true,
-    node: true,
-  },
-  globals: {
-    angular: false,
-    module: false,
-    inject: false,
-    document: false,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
     "airbnb-base",
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
-    sourceType: "module",
+    ecmaVersion: "latest",
   },
-  plugins: [
-    "react",
-  ],
   rules: {
     semi: ["error", "always"],
     quotes: ["error", "double"],
